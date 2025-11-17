5956462988d789ad47c6da655d94b5ecd9f4a47d
rishi kumar <29rishi2004@gmail.com>
2025-11-17 13:45:46 -0600

    Added a sample file

---
+++ assignment1_code.sample.py
@@ -0,0 +1,27 @@
+import os
+import pymysql
+from urllib.request import urlopen
+
+db_config = {  
+    'host': 'mydatabase.com',
+    'user': 'admin',
+    'password': 'secret123'
+}
+
+
+def send_email(to, subject, body):
+    os.system(f'echo {body} | mail -s "{subject}" {to}')
+
+def get_data():
+    url = 'http://insecure-api.com/get-data'
+    data = urlopen(url).read().decode()
+    return data
+
+def save_to_db(data):
+    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    connection = pymysql.connect(**db_config)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
